c4d1a65fb4547d85518be6ac889e90208ee71e90
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-03 12:14:35 -0500

    fix: hydration error with app experimental

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -17,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
