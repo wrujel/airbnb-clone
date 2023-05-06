1f65c1dd1c5ffe3a475803154d527057d3f9b92b
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-06 14:13:11 -0500

    feat: add loading and error page

---
+++ app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+import EmptyState from "./components/EmptyState";
+
+interface ErrorStateProps {
+  error: Error;
+}
+
+const ErrorState: React.FC<ErrorStateProps> = ({ error }) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <EmptyState
+      title="Something went wrong"
+      subtitle="Please try again later"
+    />
+  );
+};
+
+export default ErrorState;
