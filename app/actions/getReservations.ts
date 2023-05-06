2e658b7c99887e00f7ae7beb24ff9fbfcf5c266b
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-06 07:49:24 -0500

    feat: add api reservations to disabled dates in calendar

---
+++ app/actions/getReservations.ts
@@ -0,0 +1,41 @@
+import prisma from "@/app/libs/prismadb";
+
+interface Reservation {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
+}
+
+export default async function getReservations(params: Reservation) {
+  try {
+    const { listingId, userId, authorId } = params;
+
+    const query: any = {};
+
+    if (listingId) {
+      query.listingId = listingId;
+    }
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (authorId) {
+      query.listing = { userId: authorId };
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return reservations;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,6 +3,7 @@ import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
 import getCurrentUser from "@/app/actions/getCurrentUser";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingId?: string;
@@ -10,6 +11,7 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
@@ -22,7 +24,11 @@ const ListingPage = async ({ params }: { params: IParams }) => {
 
   return (
     <ClientOnly>
-      <ListingClient listing={listing} currentUser={currentUser} />
+      <ListingClient
+        listing={listing}
+        currentUser={currentUser}
+        reservations={reservations}
+      />
     </ClientOnly>
   );
 };
