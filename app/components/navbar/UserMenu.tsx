b768ea59ec5e8c5df386d0f7bb636b93483c288c
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-06 09:10:03 -0500

    feat: add improvements for register

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -101,7 +101,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                     label="My trips"
                   />
                   <MenuItem onClick={() => {}} label="My favorites" />
-                  <MenuItem onClick={() => {}} label="My reservations" />
+                  <MenuItem
+                    onClick={() => router.push("/reservations")}
+                    label="My reservations"
+                  />
                   <MenuItem onClick={() => {}} label="My properties" />
                   <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
                   <hr />
