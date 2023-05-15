e8c7e74d5895dcb3ced2b919b83cea383f478110
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-15 12:57:14 -0500

    fix: hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,8 @@ export default async function RootLayout({
           <RegisterModal />
           <LoginModal />
           <Navbar currentUser={currentUser} />
+          <div className="pb-20 pt-28">{children}</div>
         </ClientOnly>
-        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
