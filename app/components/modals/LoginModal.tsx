aba4ceb36db2edb5de923e512764917d938cf175
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2026-04-25 12:06:15 -0500

    fix: add type attribute to password input in LoginModal

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -72,6 +72,7 @@ const LoginModal = () => {
       <Input
         id="password"
         label="Password"
+        type="password"
         disabled={isLoading}
         register={register}
         errors={errors}
